2f3f4948eb04a160fa77c186cef9501ab75e6f04
ManishGowardipe <gowardipemanish95@gmail.com>
2020-04-16 16:39:35 +0530

    #000:Chauhan | Manish : Add jest, audit script and update README.md
    Co-authored-by: Shubham Chauhan <shubham25may1998@gmail.com>

---
+++ jest.config.js
@@ -0,0 +1,35 @@
+module.exports = {
+  // The root of your source code, typically /src
+  // `<rootDir>` is a token Jest substitutes
+  roots: ["<rootDir>"],
+
+  // Jest transformations -- this adds support for TypeScript
+  // using ts-jest
+  transform: {
+    "^.+\\.ts?$": "ts-jest"
+  },
+
+  // Runs special logic, such as cleaning up components
+  // when using React Testing Library and adds special
+  // extended assertions to Jest
+  // setupFilesAfterEnv: [
+  //     "@testing-library/react/cleanup-after-each",
+  //     "@testing-library/jest-dom/extend-expect"
+  // ],
+
+  // Test spec file resolution pattern
+  // Matches parent folder `__tests__` and filename
+  // should contain `test` or `spec`.
+  testRegex: "(/__tests__/.*|(\\.|/)(test|spec))\\.ts?$",
+  // coverageThreshold: {
+  //   global: {
+  //     branches: 80,
+  //     functions: 80,
+  //     lines: 80,
+  //     statements: 80
+  //   }
+  // },
+  // coverageReporters: ['json', 'lcov', 'text', 'clover'],
+  // Module file extensions for importing
+  moduleFileExtensions: ["ts", "js", "json", "node"]
+};
