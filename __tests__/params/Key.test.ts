719571f2325176a3087bcd50a0875aba448fb55d
ManishGowardipe <gowardipemanish95@gmail.com>
2020-04-20 15:23:50 +0530

    #ESW-306:Manish : add helper functions for all matrix related keys

--- __tests__/params/Key.test.ts
+++ __tests__/params/Key.test.ts
@@ -1,26 +1,29 @@
 import * as Keys from '../jsons/keys.json'
 import {
+  byteMatrixKey,
   choiceKey,
+  doubleMatrixKey,
+  floatMatrixKey,
   intArrayKey,
   intKey,
+  intMatrixKey,
+  longMatrixKey,
+  shortMatrixKey,
   stringKey,
   structKey,
   taiTimeKey,
   utcTimeKey,
 } from '../../src/params/Key'
+import { MatrixData } from '../../src/params/MatrixData'
 
 test('utc time key', () => {
-  const timeParam = utcTimeKey('utcTimeKey').set([
-    '2017-09-04T16:28:00.123456789Z',
-  ])
+  const timeParam = utcTimeKey('utcTimeKey').set(['2017-09-04T16:28:00.123456789Z'])
 
   expect(Keys.UTCTimeKey).toEqual(timeParam.toJSON().UTCTimeKey)
 })
 
 test('tai time key', () => {
-  const timeParam = taiTimeKey('taiTime').set([
-    '2017-09-04T16:28:00.123456789Z',
-  ])
+  const timeParam = taiTimeKey('taiTime').set(['2017-09-04T16:28:00.123456789Z'])
 
   expect(Keys.TAITimeKey).toEqual(timeParam.toJSON().TAITimeKey)
 })
@@ -46,13 +49,9 @@ test('int array key', () => {
 test('struct key', () => {
   const intParam = intKey('numbers').set([1, 2, 3])
   const stringParam = stringKey('days').set(['monday', 'tuesday'])
-  const structParam = structKey('structKey').set([
-    { paramSet: [intParam, stringParam] },
-  ])
+  const structParam = structKey('structKey').set([{ paramSet: [intParam, stringParam] }])
 
-  expect(Keys.StructKey).toEqual(
-    JSON.parse(JSON.stringify(structParam)).StructKey,
-  )
+  expect(Keys.StructKey).toEqual(JSON.parse(JSON.stringify(structParam)).StructKey)
 })
 
 test('choice key', () => {
@@ -61,4 +60,64 @@ test('choice key', () => {
   const choiceParam = resetKey.setChoice(choices, ['c'])
 
   expect(Keys.ChoiceKey).toEqual(choiceParam.toJSON().ChoiceKey)
+})
+
+test('int matrix key', () => {
+  const intMatrixKey1 = intMatrixKey('test matrix')
+  const data = MatrixData.fromArrays<number>([1, 2, 3], [4, 5, 6])
+
+  const matrixParam = intMatrixKey1.set([data])
+
+  const { IntMatrixKey } = matrixParam.toJSON()
+  expect(Keys.GeneralMatrixKey).toEqual(JSON.parse(JSON.stringify(IntMatrixKey)))
+})
+
+test('byte matrix key', () => {
+  const byteMatrixKey1 = byteMatrixKey('test matrix')
+  const data = MatrixData.fromArrays<number>([1, 2, 3], [4, 5, 6])
+
+  const matrixParam = byteMatrixKey1.set([data])
+
+  const { ByteMatrixKey } = matrixParam.toJSON()
+  expect(Keys.GeneralMatrixKey).toEqual(JSON.parse(JSON.stringify(ByteMatrixKey)))
+})
+
+test('long matrix key', () => {
+  const longMatrixKey1 = longMatrixKey('test matrix')
+  const data = MatrixData.fromArrays<number>([1, 2, 3], [4, 5, 6])
+
+  const matrixParam = longMatrixKey1.set([data])
+
+  const { LongMatrixKey } = matrixParam.toJSON()
+  expect(Keys.GeneralMatrixKey).toEqual(JSON.parse(JSON.stringify(LongMatrixKey)))
+})
+
+test('short matrix key', () => {
+  const shortMatrixKey1 = shortMatrixKey('test matrix')
+  const data = MatrixData.fromArrays<number>([1, 2, 3], [4, 5, 6])
+
+  const matrixParam = shortMatrixKey1.set([data])
+
+  const { ShortMatrixKey } = matrixParam.toJSON()
+  expect(Keys.GeneralMatrixKey).toEqual(JSON.parse(JSON.stringify(ShortMatrixKey)))
+})
+
+test('float matrix key', () => {
+  const floatMatrixKey1 = floatMatrixKey('test matrix')
+  const data = MatrixData.fromArrays<number>([1.0, 2.2, 3.3], [4.444, 5.34, 6.77])
+
+  const matrixParam = floatMatrixKey1.set([data])
+
+  const { FloatMatrixKey } = matrixParam.toJSON()
+  expect(Keys.FractionMatrixKey).toEqual(JSON.parse(JSON.stringify(FloatMatrixKey)))
+})
+
+test('double matrix key', () => {
+  const doubleMatrixKey1 = doubleMatrixKey('test matrix')
+  const data = MatrixData.fromArrays<number>([1.0, 2.2, 3.3], [4.444, 5.34, 6.77])
+
+  const matrixParam = doubleMatrixKey1.set([data])
+
+  const { DoubleMatrixKey } = matrixParam.toJSON()
+  expect(Keys.FractionMatrixKey).toEqual(JSON.parse(JSON.stringify(DoubleMatrixKey)))
 })
