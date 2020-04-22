616c2a00281c519d3cfc129f2a49ba7989e5ccee
Pritam Kadam <phkadam2008@gmail.com>
2020-04-22 20:07:25 +0530

    Include tests in ts compilation scope so that IDE understands absolute path imports in tests

--- jest.config.js
+++ jest.config.js
@@ -1,26 +1,12 @@
 module.exports = {
-  // The root of your source code, typically /src
-  // `<rootDir>` is a token Jest substitutes
   roots: ['<rootDir>'],
 
-  // Jest transformations -- this adds support for TypeScript
-  // using ts-jest
+  // Jest transformations -- this adds support for TypeScript using ts-jest
   transform: {
     '^.+\\.ts?$': 'ts-jest',
   },
 
-  // Runs special logic, such as cleaning up components
-  // when using React Testing Library and adds special
-  // extended assertions to Jest
-  // setupFilesAfterEnv: [
-  //     "@testing-library/react/cleanup-after-each",
-  //     "@testing-library/jest-dom/extend-expect"
-  // ],
-
-  // Test spec file resolution pattern
-  // Matches parent folder `__tests__` and filename
-  // should contain `test` or `spec`.
-  testRegex: '(/__tests__/.*|(\\.|/)(test|spec))\\.ts?$',
+  testRegex: '(/src/__tests__/.*|(\\.|/)(test|spec))\\.ts?$',
   // coverageThreshold: {
   //   global: {
   //     branches: 80,
@@ -30,7 +16,7 @@ module.exports = {
   //   }
   // },
   // coverageReporters: ['json', 'lcov', 'text', 'clover'],
-  // Module file extensions for importing
+  
   moduleFileExtensions: ['ts', 'js', 'json', 'node'],
   moduleDirectories: ['node_modules', 'src'],
 }
