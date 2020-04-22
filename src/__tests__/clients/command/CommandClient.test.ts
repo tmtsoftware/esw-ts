e10e981749b2669717e38052a3c6f74755cb6650
ManishGowardipe <gowardipemanish95@gmail.com>
2020-04-22 14:54:11 +0530

    #ESW-305 :Manish | chauhan : Add oneway and query api to command client
    Co-authored-by: Shubham Chauhan <shubham25may1998@gmail.com>

--- src/__tests__/clients/command/CommandClient.test.ts
+++ src/__tests__/clients/command/CommandClient.test.ts
@@ -1,21 +1,27 @@
 import { CommandClient } from 'clients/command/CommandClient'
 import { ComponentId } from 'models/ComponentId'
 import { Http } from 'utils/Http'
-import { ControlCommand } from 'clients/command/models/PostCommand'
-import { SubmitResponse, ValidateResponse } from 'clients/command/models/CommandResponse'
+import { ControlCommand, QueryCommand } from 'clients/command/models/PostCommand'
+import {
+  OneWayResponse,
+  SubmitResponse,
+  ValidateResponse,
+} from 'clients/command/models/CommandResponse'
 import { Prefix } from 'models/params/Prefix'
 
 const mockFn = jest.fn()
+
+const compId: ComponentId = {
+  prefix: new Prefix('ESW', 'test'),
+  componentType: 'Assembly',
+}
+const client = CommandClient('localhost', 1234, compId)
 
 beforeAll(() => {
   Http.post = mockFn
 })
 
 test('it should post validate command', async () => {
-  const compId: ComponentId = {
-    prefix: new Prefix('ESW', 'test'),
-    componentType: 'Assembly',
-  }
   const acceptedResponse = {
     _type: 'Accepted',
     runId: '1234124',
@@ -23,7 +29,6 @@ test('it should post validate command', async () => {
 
   mockFn.mockReturnValueOnce(acceptedResponse)
 
-  const client = CommandClient('localhost', 1234, compId)
   const controlCommand: ControlCommand = getControlCommand()
   const data: ValidateResponse = await client.validate(controlCommand)
 
@@ -32,23 +37,51 @@ test('it should post validate command', async () => {
 })
 
 test('it should post submit command', async () => {
-  const compId: ComponentId = {
-    prefix: new Prefix('ESW', 'test'),
-    componentType: 'Assembly',
-  }
-  const acceptedResponse = {
+  const startedResponse = {
     _type: 'Started',
+    runId: '1234124',
+  }
+
+  mockFn.mockReturnValueOnce(startedResponse)
+
+  const controlCommand: ControlCommand = getControlCommand()
+  const data: SubmitResponse = await client.submit(controlCommand)
+
+  expect(mockFn).toBeCalledTimes(1)
+  expect(data).toBe(startedResponse)
+})
+
+test('it should post oneway command', async () => {
+  const acceptedResponse = {
+    _type: 'Accepted',
     runId: '1234124',
   }
 
   mockFn.mockReturnValueOnce(acceptedResponse)
 
-  const client = CommandClient('localhost', 1234, compId)
   const controlCommand: ControlCommand = getControlCommand()
-  const data: SubmitResponse = await client.submit(controlCommand)
+  const data: OneWayResponse = await client.oneway(controlCommand)
 
   expect(mockFn).toBeCalledTimes(1)
   expect(data).toBe(acceptedResponse)
+})
+
+test('it should post query command', async () => {
+  const completedResponse = {
+    _type: 'Completed',
+    runId: '1234124',
+  }
+
+  mockFn.mockReturnValueOnce(completedResponse)
+
+  const queryCommand: QueryCommand = {
+    _type: 'Query',
+    runId: '1234124',
+  }
+  const data: SubmitResponse = await client.query(queryCommand)
+
+  expect(mockFn).toBeCalledTimes(1)
+  expect(data).toBe(completedResponse)
 })
 
 function getControlCommand(): ControlCommand {
