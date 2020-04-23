11cbaf0108628b7121027cf1f081ea9c8e4a55fc
ManishGowardipe <gowardipemanish95@gmail.com>
2020-04-23 13:59:17 +0530

    #ESW-305 :Manish | chauhan : export post as a function instead of object
    Co-authored-by: Shubham Chauhan <shubham25may1998@gmail.com>

--- src/clients/command/CommandClient.ts
+++ src/clients/command/CommandClient.ts
@@ -1,5 +1,5 @@
 import { ControlCommand } from 'clients/command/models/PostCommand'
-import { Http } from 'utils/Http'
+
 import { GatewayCommand, GatewayCommandType } from 'clients/command/models/GatewayCommand'
 import { ComponentId } from 'models/ComponentId'
 import {
@@ -14,6 +14,7 @@ import {
   WebsocketCommand
 } from './models/WsCommand'
 import { CurrentState } from 'models/params/CurrentState'
+import { post } from 'utils/Http'
 import { ComponentCommandFactory } from 'clients/command/ComponentCommandFactory'
 
 export interface CommandClient {
@@ -46,18 +47,18 @@ export const CommandClient = (
   componentId: ComponentId
 ): CommandClient => {
   const commandFactory = new ComponentCommandFactory(componentId)
-  const post = <T>(gatewayCommand: GatewayCommand) => Http.post<T>(host, port, gatewayCommand)
+  const httpPost = <T>(gatewayCommand: GatewayCommand) => post<T>(host, port, gatewayCommand)
 
   const validate = async (command: ControlCommand) =>
-    post<ValidateResponse>(commandFactory.validate(command))
+    httpPost<ValidateResponse>(commandFactory.validate(command))
 
   const submit = async (command: ControlCommand) =>
-    post<SubmitResponse>(commandFactory.submit(command))
+    httpPost<SubmitResponse>(commandFactory.submit(command))
 
   const oneway = async (command: ControlCommand) =>
-    post<OneWayResponse>(commandFactory.oneway(command))
+    httpPost<OneWayResponse>(commandFactory.oneway(command))
 
-  const query = async (runId: string) => post<SubmitResponse>(commandFactory.query(runId))
+  const query = async (runId: string) => httpPost<SubmitResponse>(commandFactory.query(runId))
 
   const subscribeCurrentState = (
     stateNames: Set<string>,
