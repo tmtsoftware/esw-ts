69ead58db9b5ff3504d66168f47f2d74be2dd803
Pritam Kadam <phkadam2008@gmail.com>
2020-04-27 20:51:29 +0530

    Remove ComponentCommandFactory

--- src/clients/command/CommandService.ts
+++ src/clients/command/CommandService.ts
@@ -1,11 +1,12 @@
+import { Oneway, Query, Submit, Validate } from 'clients/command/models/PostCommand'
+import { QueryFinal, SubscribeCurrentState } from 'clients/command/models/WsCommand'
+import { GatewayComponentCommand } from 'clients/gateway/models/Gateway'
 import { ComponentId } from 'models/ComponentId'
+import { ControlCommand } from 'models/params/Command'
 import { OneWayResponse, SubmitResponse, ValidateResponse } from 'models/params/CommandResponse'
-import { Subscription, Ws } from 'utils/Ws'
 import { CurrentState } from 'models/params/CurrentState'
 import { post } from 'utils/Http'
-import { ComponentCommandFactory } from 'clients/command/ComponentCommandFactory'
-import { ControlCommand } from 'models/params/Command'
-import { GatewayComponentCommand } from 'clients/gateway/models/Gateway'
+import { Subscription, Ws } from 'utils/Ws'
 
 export interface CommandService {
   validate(command: ControlCommand): Promise<ValidateResponse>
@@ -25,34 +26,34 @@ export const CommandService = (
   port: number,
   componentId: ComponentId
 ): CommandService => {
-  const commandFactory = new ComponentCommandFactory(componentId)
   const httpPost = <T>(gatewayCommand: GatewayComponentCommand) =>
     post<GatewayComponentCommand, T>(host, port, gatewayCommand)
 
   const validate = async (command: ControlCommand) =>
-    httpPost<ValidateResponse>(commandFactory.validate(command))
+    httpPost<ValidateResponse>(GatewayComponentCommand(componentId, Validate(command)))
 
   const submit = async (command: ControlCommand) =>
-    httpPost<SubmitResponse>(commandFactory.submit(command))
+    httpPost<SubmitResponse>(GatewayComponentCommand(componentId, Submit(command)))
 
   const oneway = async (command: ControlCommand) =>
-    httpPost<OneWayResponse>(commandFactory.oneway(command))
+    httpPost<OneWayResponse>(GatewayComponentCommand(componentId, Oneway(command)))
 
-  const query = async (runId: string) => httpPost<SubmitResponse>(commandFactory.query(runId))
+  const query = async (runId: string) =>
+    httpPost<SubmitResponse>(GatewayComponentCommand(componentId, Query(runId)))
 
   const subscribeCurrentState = (
     stateNames: Set<string>,
     onStateChange: (state: CurrentState) => void
   ): Subscription =>
     new Ws(host, port).sendThenSubscribe(
-      commandFactory.subscribeCurrentState(stateNames),
+      GatewayComponentCommand(componentId, SubscribeCurrentState(stateNames)),
       onStateChange
     )
 
   const queryFinal = async (runId: string, timeoutInSeconds: number) =>
     new Promise<SubmitResponse>((resolve) => {
       new Ws(host, port).sendThenSubscribe(
-        commandFactory.queryFinal(runId, timeoutInSeconds),
+        GatewayComponentCommand(componentId, QueryFinal(runId, timeoutInSeconds)),
         (submitResponse: SubmitResponse) => {
           resolve(submitResponse)
         }
