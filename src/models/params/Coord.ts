ec341ec00ca5ac46116db03419005573a7576386
Pritam Kadam <phkadam2008@gmail.com>
2020-04-30 21:20:43 +0530

    Mark interfaces properties readonly in models

--- src/models/params/Coord.ts
+++ src/models/params/Coord.ts
@@ -25,62 +25,62 @@ export type SolarSystemObject =
 export type EqFrame = 'ICRS' | 'FK5'
 
 export interface RaDec {
-  ra: number
-  dec: number
+  readonly ra: number
+  readonly dec: number
 }
 
 export interface ProperMotion {
-  pmx: number
-  pmy: number
+  readonly pmx: number
+  readonly pmy: number
 }
 
 export interface EqCoord extends Coord {
-  _type: 'EqCoord'
-  tag: Tag
-  ra: number
-  dec: number
-  frame: EqFrame
-  catalogName: string
-  pm: ProperMotion
+  readonly _type: 'EqCoord'
+  readonly tag: Tag
+  readonly ra: number
+  readonly dec: number
+  readonly frame: EqFrame
+  readonly catalogName: string
+  readonly pm: ProperMotion
 }
 
 export interface MinorPlanetCoord extends Coord {
-  _type: 'MinorPlanetCoord'
-  tag: Tag
-  epoch: number
-  inclination: number
-  longAscendingNode: number
-  argOfPerihelion: number
-  meanDistance: number
-  eccentricity: number
-  meanAnomaly: number
+  readonly _type: 'MinorPlanetCoord'
+  readonly tag: Tag
+  readonly epoch: number
+  readonly inclination: number
+  readonly longAscendingNode: number
+  readonly argOfPerihelion: number
+  readonly meanDistance: number
+  readonly eccentricity: number
+  readonly meanAnomaly: number
 }
 
 export interface SolarSystemCoord extends Coord {
-  _type: 'SolarSystemCoord'
-  tag: Tag
-  body: SolarSystemObject
+  readonly _type: 'SolarSystemCoord'
+  readonly tag: Tag
+  readonly body: SolarSystemObject
 }
 
 export interface CometCoord extends Coord {
-  _type: 'CometCoord'
-  tag: Tag
-  epochOfPerihelion: number
-  inclination: number
-  longAscendingNode: number
-  argOfPerihelion: number
-  perihelionDistance: number
-  eccentricity: number
+  readonly _type: 'CometCoord'
+  readonly tag: Tag
+  readonly epochOfPerihelion: number
+  readonly inclination: number
+  readonly longAscendingNode: number
+  readonly argOfPerihelion: number
+  readonly perihelionDistance: number
+  readonly eccentricity: number
 }
 
 export interface AltAzCoord extends Coord {
-  _type: 'AltAzCoord'
-  tag: Tag
-  alt: number
-  az: number
+  readonly _type: 'AltAzCoord'
+  readonly tag: Tag
+  readonly alt: number
+  readonly az: number
 }
 
 export interface Coord {
-  _type: string
-  tag: Tag
+  readonly _type: string
+  readonly tag: Tag
 }
