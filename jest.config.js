404ea78f7d9eec64b8e5f37cfa179271c6092418
ManishGowardipe <gowardipemanish95@gmail.com>
2020-05-01 00:09:54 +0530

    Ignore dist files in jest config and add declaration true for tsconfig

--- jest.config.js
+++ jest.config.js
@@ -2,7 +2,7 @@ module.exports = {
   roots: ['<rootDir>'],
   preset: 'ts-jest',
   testRegex: '(/test/.*|(\\.|/)(test|spec))\\.ts?$',
-  testPathIgnorePatterns: ['test/utils/'],
+  testPathIgnorePatterns: ['test/utils/', 'dist/'],
   moduleFileExtensions: ['ts', 'js', 'json', 'node'],
   moduleDirectories: ['node_modules', 'src', 'test']
 }
