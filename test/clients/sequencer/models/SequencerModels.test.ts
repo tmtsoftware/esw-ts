91ce7f96940eb38d4f5bd1ba45b0ab3aacf13ad6
Pritam Kadam <phkadam2008@gmail.com>
2020-05-02 12:27:17 +0530

    Convert Sequencer test to data driven based

--- test/clients/sequencer/models/SequencerModels.test.ts
+++ test/clients/sequencer/models/SequencerModels.test.ts
@@ -1,155 +1,33 @@
-import {
-  AbortSequence,
-  Add,
-  AddBreakpoint,
-  Delete,
-  DiagnosticMode,
-  GetSequence,
-  GoOffline,
-  GoOnline,
-  InsertAfter,
-  IsAvailable,
-  IsOnline,
-  LoadSequence,
-  OperationsMode,
-  Pause,
-  Prepend,
-  RemoveBreakpoint,
-  Replace,
-  Reset,
-  Resume,
-  StartSequence,
-  Stop
-} from 'clients/sequencer/models/PostCommand'
-import * as SequencerCommands from 'jsons/sequencerModels.json'
+import * as Sequencer from 'clients/sequencer/models/PostCommand'
+import * as TestData from 'jsons/sequencerModels.json'
 import { SequenceCommand, Setup } from 'models/params/Command'
 
 const setupCommand = new Setup('ESW.test', 'command-1', [])
 const sequence: SequenceCommand[] = [setupCommand]
 const commands: SequenceCommand[] = [setupCommand]
+const id = 'id-1234'
+const date = new Date('2020-10-08')
 
-test('LoadSequence', () => {
-  const loadSequence = new LoadSequence(sequence)
-
-  expect(loadSequence).toEqual(SequencerCommands.LoadSequence)
-})
-
-test('StartSequence', () => {
-  const startSequence = new StartSequence()
-
-  expect(startSequence).toEqual(SequencerCommands.StartSequence)
-})
-
-test('Resume', () => {
-  const resume: Resume = new Resume()
-
-  expect(resume).toEqual(SequencerCommands.Resume)
-})
-
-test('Reset', () => {
-  const reset: Reset = new Reset()
-
-  expect(reset).toEqual(SequencerCommands.Reset)
-})
-
-test('Pause', () => {
-  const pause: Pause = new Pause()
-
-  expect(pause).toEqual(SequencerCommands.Pause)
-})
-
-test('Add', () => {
-  const add: Add = new Add(commands)
-
-  expect(add).toEqual(SequencerCommands.Add)
-})
-
-test('Prepend', () => {
-  const prepend: Prepend = new Prepend(commands)
-
-  expect(prepend).toEqual(SequencerCommands.Prepend)
-})
-
-test('Replace', () => {
-  const replace: Replace = new Replace('id-1234', commands)
-
-  expect(replace).toEqual(SequencerCommands.Replace)
-})
-
-test('InsertAfter', () => {
-  const insertAfter: InsertAfter = new InsertAfter('id-1234', commands)
-
-  expect(insertAfter).toEqual(SequencerCommands.InsertAfter)
-})
-
-test('Delete', () => {
-  const deleteCommand: Delete = new Delete('id-1234')
-
-  expect(deleteCommand).toEqual(SequencerCommands.Delete)
-})
-
-test('AddBreakpoint', () => {
-  const addBreakpoint: AddBreakpoint = new AddBreakpoint('id-1234')
-
-  expect(addBreakpoint).toEqual(SequencerCommands.AddBreakpoint)
-})
-
-test('RemoveBreakpoint', () => {
-  const removeBreakpoint: RemoveBreakpoint = new RemoveBreakpoint('id-1234')
-
-  expect(removeBreakpoint).toEqual(SequencerCommands.RemoveBreakpoint)
-})
-
-test('GetSequence', () => {
-  const getSequence: GetSequence = new GetSequence()
-
-  expect(getSequence).toEqual(SequencerCommands.GetSequence)
-})
-
-test('IsAvailable', () => {
-  const isAvailable: IsAvailable = new IsAvailable()
-
-  expect(isAvailable).toEqual(SequencerCommands.IsAvailable)
-})
-
-test('IsOnline', () => {
-  const isOnline: IsOnline = new IsOnline()
-
-  expect(isOnline).toEqual(SequencerCommands.IsOnline)
-})
-
-test('GoOnline', () => {
-  const goOnline: GoOnline = new GoOnline()
-
-  expect(goOnline).toEqual(SequencerCommands.GoOnline)
-})
-
-test('GoOffline', () => {
-  const goOffline: GoOffline = new GoOffline()
-
-  expect(goOffline).toEqual(SequencerCommands.GoOffline)
-})
-
-test('AbortSequence', () => {
-  const abortSequence: AbortSequence = new AbortSequence()
-
-  expect(abortSequence).toEqual(SequencerCommands.AbortSequence)
-})
-
-test('Stop', () => {
-  const stop: Stop = new Stop()
-
-  expect(stop).toEqual(SequencerCommands.Stop)
-})
-
-test('OperationsMode', () => {
-  const operationsMode: OperationsMode = new OperationsMode()
-
-  expect(operationsMode).toEqual(SequencerCommands.OperationsMode)
-})
-
-test('DiagnosticMode', () => {
-  const diagnosticMode: DiagnosticMode = new DiagnosticMode(new Date('2020-10-08'), 'start')
-
-  expect(diagnosticMode.toJSON()).toEqual(SequencerCommands.DiagnosticMode)
-})
+test.each([
+  ['LoadSequence', new Sequencer.LoadSequence(sequence), TestData.LoadSequence],
+  ['StartSequence', new Sequencer.StartSequence(), TestData.StartSequence],
+  ['Resume', new Sequencer.Resume(), TestData.Resume],
+  ['Reset', new Sequencer.Reset(), TestData.Reset],
+  ['Pause', new Sequencer.Pause(), TestData.Pause],
+  ['Add', new Sequencer.Add(commands), TestData.Add],
+  ['Prepend', new Sequencer.Prepend(commands), TestData.Prepend],
+  ['Replace', new Sequencer.Replace(id, commands), TestData.Replace],
+  ['InsertAfter', new Sequencer.InsertAfter(id, commands), TestData.InsertAfter],
+  ['Delete', new Sequencer.Delete(id), TestData.Delete],
+  ['AddBreakpoint', new Sequencer.AddBreakpoint(id), TestData.AddBreakpoint],
+  ['RemoveBreakpoint', new Sequencer.RemoveBreakpoint(id), TestData.RemoveBreakpoint],
+  ['GetSequence', new Sequencer.GetSequence(), TestData.GetSequence],
+  ['IsAvailable', new Sequencer.IsAvailable(), TestData.IsAvailable],
+  ['IsOnline', new Sequencer.IsOnline(), TestData.IsOnline],
+  ['GoOnline', new Sequencer.GoOnline(), TestData.GoOnline],
+  ['GoOffline', new Sequencer.GoOffline(), TestData.GoOffline],
+  ['AbortSequence', new Sequencer.AbortSequence(), TestData.AbortSequence],
+  ['Stop', new Sequencer.Stop(), TestData.Stop],
+  ['OperationsMode', new Sequencer.OperationsMode(), TestData.OperationsMode],
+  ['DiagnosticMode', new Sequencer.DiagnosticMode(date, 'start'), TestData.DiagnosticMode]
+])('%s', (_, actual, expected) => expect(JSON.parse(JSON.stringify(actual))).toEqual(expected))
