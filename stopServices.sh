e27ad577709118ceb6b8a42496d2c4ee7477d95a
Pritam Kadam <phkadam2008@gmail.com>
2020-05-20 15:35:46 +0530

    Use cs native  client if present on machine

--- stopServices.sh
+++ stopServices.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
-TMT_BACKEND_LIB=com.github.tmtsoftware.tmt-backend:tmt-backend_2.13
-jps -m | grep $TMT_BACKEND_LIB | awk '{print $1}' | xargs kill
+jps -m | grep BackendService | awk '{print $1}' | xargs kill
+jps -m | grep ContainerCmdApp | awk '{print $1}' | xargs kill
