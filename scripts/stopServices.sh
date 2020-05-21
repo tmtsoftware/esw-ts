6f950678b12be0612cba384bea5ea1dd5e78c13f
ManishGowardipe <gowardipemanish95@gmail.com>
2020-05-21 16:15:34 +0530

    update stop services script

--- scripts/stopServices.sh
+++ scripts/stopServices.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
-jps -m
+jps -m | grep "backend-testkit-component" | awk '{print $1}' | xargs kill
+jps -m | grep "backend-testkit-services" | awk '{print $1}' | xargs kill
 jps -m | grep ContainerCmdApp | awk '{print $1}' | xargs kill
 jps -m | grep BackendService | awk '{print $1}' | xargs kill
