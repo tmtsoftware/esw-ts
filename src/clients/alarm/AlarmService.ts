44603a76fd1844acc48844288b49a018518f0826
Pritam Kadam <phkadam2008@gmail.com>
2020-05-30 23:24:34 +0530

    #000: Pritam: Extract common code in CommandService

--- src/clients/alarm/AlarmService.ts
+++ src/clients/alarm/AlarmService.ts
@@ -1,7 +1,7 @@
+import { AlarmKey, AlarmSeverity, SetAlarmSeverity } from 'clients/alarm/models/PostCommand'
+import { resolveGateway } from 'clients/gateway/resolveGateway'
 import { Done } from 'clients/location'
 import { HttpTransport } from 'utils/HttpTransport'
-import { resolveGateway } from 'clients/gateway/resolveGateway'
-import { AlarmKey, AlarmSeverity, SetAlarmSeverity } from 'clients/alarm/models/PostCommand'
 
 interface AlarmApi {
   setSeverity(alarmKey: AlarmKey, severity: AlarmSeverity): Promise<Done>
@@ -9,6 +9,7 @@ interface AlarmApi {
 
 export class AlarmService implements AlarmApi {
   private readonly httpTransport: HttpTransport<SetAlarmSeverity>
+
   constructor() {
     this.httpTransport = new HttpTransport<SetAlarmSeverity>(resolveGateway)
   }
--- src/clients/command/CommandService.ts
+++ src/clients/command/CommandService.ts
@@ -1,15 +1,5 @@
-import {
-  CommandServiceHttpMessage,
-  Oneway,
-  Query,
-  Submit,
-  Validate
-} from 'clients/command/models/PostCommand'
-import {
-  CommandServiceWsMessage,
-  QueryFinal,
-  SubscribeCurrentState
-} from 'clients/command/models/WsCommand'
+import * as Req from 'clients/command/models/PostCommand'
+import * as WsReq from 'clients/command/models/WsCommand'
 import { GatewayComponentCommand } from 'clients/gateway/models/Gateway'
 import { resolveGateway } from 'clients/gateway/resolveGateway'
 import { ComponentId } from 'models/ComponentId'
@@ -39,49 +29,58 @@ export class CommandService implements CommandServiceApi {
     this.httpTransport = new HttpTransport(resolveGateway, this.tokenFactory)
   }
 
-  private componentCommand(msg: CommandServiceHttpMessage | CommandServiceWsMessage) {
+  private componentCommand(msg: Req.CommandServiceHttpMessage | WsReq.CommandServiceWsMessage) {
     return new GatewayComponentCommand(this.componentId, msg)
   }
 
+  private postComponentCmd<Res>(msg: Req.CommandServiceHttpMessage) {
+    return this.httpTransport.requestRes<Res>(this.componentCommand(msg))
+  }
+
   validate(command: ControlCommand): Promise<ValidateResponse> {
-    return this.httpTransport.requestRes<ValidateResponse>(
-      this.componentCommand(new Validate(command))
-    )
+    return this.postComponentCmd(new Req.Validate(command))
   }
 
   submit(command: ControlCommand): Promise<SubmitResponse> {
-    return this.httpTransport.requestRes<SubmitResponse>(this.componentCommand(new Submit(command)))
+    return this.postComponentCmd(new Req.Submit(command))
   }
 
   oneway(command: ControlCommand): Promise<OneWayResponse> {
-    return this.httpTransport.requestRes<OneWayResponse>(this.componentCommand(new Oneway(command)))
+    return this.postComponentCmd(new Req.Oneway(command))
   }
 
   query(runId: string): Promise<SubmitResponse> {
-    return this.httpTransport.requestRes<SubmitResponse>(this.componentCommand(new Query(runId)))
+    return this.postComponentCmd(new Req.Query(runId))
+  }
+
+  private async ws(): Promise<Ws<GatewayComponentCommand>> {
+    const { host, port } = await resolveGateway()
+    return new Ws(host, port)
+  }
+
+  private async subscribe(stateNames: Set<string>, onStateChange: (state: CurrentState) => void) {
+    return (await this.ws()).subscribe(
+      this.componentCommand(new WsReq.SubscribeCurrentState(stateNames)),
+      onStateChange
+    )
   }
 
   subscribeCurrentState(
     stateNames: Set<string>,
     onStateChange: (state: CurrentState) => void
   ): Subscription {
-    const subscriptionResponse = resolveGateway().then(({ host, port }) =>
-      new Ws(host, port).subscribe(
-        this.componentCommand(new SubscribeCurrentState(stateNames)),
-        onStateChange
-      )
-    )
+    const subscriptionResponse = this.subscribe(stateNames, onStateChange)
     return {
-      cancel: () => {
-        return subscriptionResponse.then((response) => response.cancel())
+      cancel: async () => {
+        const response = await subscriptionResponse
+        return response.cancel()
       }
     }
   }
 
   async queryFinal(runId: string, timeoutInSeconds: number): Promise<SubmitResponse> {
-    const { host, port } = await resolveGateway()
-    return new Ws(host, port).singleResponse(
-      this.componentCommand(new QueryFinal(runId, timeoutInSeconds))
+    return (await this.ws()).singleResponse(
+      this.componentCommand(new WsReq.QueryFinal(runId, timeoutInSeconds))
     )
   }
 }
