e3e019f5739edfaf68abfd6a5b6bc79ec354a5ff
Pritam Kadam <phkadam2008@gmail.com>
2020-05-30 23:04:59 +0530

    #000: Pritam: Add missing APIs in LocationServiceApi

--- src/clients/location/LocationService.ts
+++ src/clients/location/LocationService.ts
@@ -1,16 +1,7 @@
 import { Connection, ConnectionType } from 'clients/location/models/Connection'
 import { Location } from 'clients/location/models/Location'
 import { Done } from 'clients/location/models/LocationResponses'
-import {
-  ListByComponentType,
-  ListByConnectionType,
-  ListByHostname,
-  ListByPrefix,
-  ListEntries,
-  LocationHttpMessage,
-  Resolve,
-  Unregister
-} from 'clients/location/models/PostCommand'
+import * as Req from 'clients/location/models/PostCommand'
 import { TrackingEvent } from 'clients/location/models/TrackingEvent'
 import { Track } from 'clients/location/models/WsCommand'
 import { LocationConfig } from 'config/LocationConfig'
@@ -22,12 +13,16 @@ import { Subscription, Ws } from 'utils/Ws'
 export interface LocationServiceApi {
   list(): Promise<Location[]>
   listByComponentType(componentType: ComponentType): Promise<Location[]>
+  listByHostname(hostname: string): Promise<Location[]>
+  listByConnectionType(connectionType: ConnectionType): Promise<Location[]>
+  listByPrefix(prefix: Prefix): Promise<Location[]>
   unregister(connection: Connection): Promise<Done>
-  resolve(connection: Connection, within: number): Promise<Location[]>
+  resolve(connection: Connection, withinSeconds: number): Promise<Location[]>
+  track(connection: Connection, callBack: (trackingEvent: TrackingEvent) => void): Subscription
 }
 
 export class LocationService implements LocationServiceApi {
-  private readonly httpTransport: HttpTransport<LocationHttpMessage>
+  private readonly httpTransport: HttpTransport<Req.LocationHttpMessage>
 
   constructor(
     readonly host: string = LocationConfig.hostName,
@@ -37,34 +32,33 @@ export class LocationService implements LocationServiceApi {
   }
 
   list(): Promise<Location[]> {
-    return this.httpTransport.requestRes<Location[]>(new ListEntries())
+    return this.httpTransport.requestRes(new Req.ListEntries())
   }
 
   listByComponentType(componentType: ComponentType): Promise<Location[]> {
-    return this.httpTransport.requestRes<Location[]>(new ListByComponentType(componentType))
+    return this.httpTransport.requestRes(new Req.ListByComponentType(componentType))
   }
 
   listByHostname(hostname: string): Promise<Location[]> {
-    return this.httpTransport.requestRes<Location[]>(new ListByHostname(hostname))
+    return this.httpTransport.requestRes(new Req.ListByHostname(hostname))
   }
 
   listByConnectionType(connectionType: ConnectionType): Promise<Location[]> {
-    return this.httpTransport.requestRes<Location[]>(new ListByConnectionType(connectionType))
+    return this.httpTransport.requestRes(new Req.ListByConnectionType(connectionType))
   }
 
   listByPrefix(prefix: Prefix): Promise<Location[]> {
-    return this.httpTransport.requestRes<Location[]>(new ListByPrefix(prefix))
+    return this.httpTransport.requestRes(new Req.ListByPrefix(prefix))
   }
 
   unregister(connection: Connection): Promise<Done> {
-    return this.httpTransport.requestRes<Done>(new Unregister(connection))
+    return this.httpTransport.requestRes(new Req.Unregister(connection))
   }
 
   //todo: decide on within parameter to be in seconds or custom time interval
+  // add threshold check, take into consideration of http connection timeout at os/network layer
   resolve(connection: Connection, withinSeconds: number): Promise<Location[]> {
-    return this.httpTransport.requestRes<Location[]>(
-      new Resolve(connection, `${withinSeconds} seconds`)
-    )
+    return this.httpTransport.requestRes(new Req.Resolve(connection, `${withinSeconds} seconds`))
   }
 
   track(connection: Connection, callBack: (trackingEvent: TrackingEvent) => void): Subscription {
