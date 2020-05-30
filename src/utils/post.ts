176ab6a2a81f481afd099238261f043e6b5c8e05
Pritam Kadam <phkadam2008@gmail.com>
2020-05-30 20:00:11 +0530

    #000: Pritam: Remove unnecessary types from post.ts

--- src/utils/post.ts
+++ src/utils/post.ts
@@ -1,22 +1,20 @@
 import 'whatwg-fetch'
 import { HeaderExt } from 'utils/HeaderExt'
 
-type RequestConfig = {
-  method: string
-  headers: Headers
-  body: string
-}
-
 export const post = async <Req, Res>(
   hostname: string,
   port: number,
   payload: Req,
   path = '',
   headers: Headers = new HeaderExt().withContentType('application/json')
-): Promise<Res> => {
-  const url = `http://${hostname}:${port}/${path}`
-  return clientFetch(url, payload, 'POST', headers)
-}
+): Promise<Res> =>
+  handleErrors(
+    await fetch(`http://${hostname}:${port}/${path}`, {
+      method: 'POST',
+      headers: headers,
+      body: bodySerializer(getContentType(headers))(payload)
+    })
+  ).json()
 
 const handleErrors = (res: Response) => {
   if (!res.ok) throw new Error(res.statusText)
@@ -33,21 +31,4 @@ const serializers = new Map([
 ])
 
 const getContentType = (headers: Headers) => headers.get('Content-Type') || 'application/json'
-
-const bodySerializer = (headers: Headers) =>
-  serializers.get(getContentType(headers)) || JSON.stringify
-
-const clientFetch = async <Req, T>(
-  url: string,
-  payload: Req,
-  method: 'POST' | 'GET',
-  headers: Headers
-): Promise<T> => {
-  const request: RequestConfig = {
-    method,
-    headers,
-    body: bodySerializer(headers)(payload)
-  }
-
-  return handleErrors(await fetch(url, request)).json()
-}
+const bodySerializer = (contentType: string) => serializers.get(contentType) || JSON.stringify
