db45db1feb4ca4803bc28cb4a17f5a866f2ec247
ManishGowardipe <gowardipemanish95@gmail.com>
2020-06-15 19:33:04 +0530

    #000 - Manish | Himanshu : fix package.lock json and resolve gateway logic
    Co-authored-by: Himanshu mertia <mertia.himanshu@gmail.com>

--- aas-examples/config-app/src/config/ResolveConfig.js
+++ aas-examples/config-app/src/config/ResolveConfig.js
@@ -1,9 +1,10 @@
 import {HttpConnection, LocationService, Prefix} from 'gateway-tsclient'
+import {Duration} from "gateway-tsclient";
 
 export const resolveConfig = async () => {
   let httpConnection = new HttpConnection(new Prefix('CSW', 'ConfigServer'), "Service");
   const locationService = new LocationService()
-  const configServerLocation = await locationService.resolve(httpConnection, 10)
+  const configServerLocation = await locationService.resolve(httpConnection, new Duration(10, 'seconds'))
   if(configServerLocation.length < 1) console.log('config server not found')
   return configServerLocation[0].uri
 }
