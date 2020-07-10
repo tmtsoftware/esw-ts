fb689d6b45ffece0d3c89f8cb5d809f7a54845e0
Pritam Kadam <phkadam2008@gmail.com>
2020-07-11 02:54:13 +0530

    Add runtime types for all the keys

--- lib/src/models/params/BaseKey.ts
+++ lib/src/models/params/BaseKey.ts
@@ -1,11 +1,10 @@
-import { Parameter } from './Parameter'
+import { Key, Parameter } from './Parameter'
 import { Units } from './Units'
-import { Key, KeyTag, KeyType } from './Key'
 
 export class BaseKey<T extends Key> {
-  constructor(readonly keyName: string, readonly keyTag: KeyTag<T>, readonly units: Units) {}
+  constructor(readonly keyName: string, readonly keyTag: T['KeyTag'], readonly units: Units) {}
 
-  set(values: KeyType<T>[]): Parameter<T> {
+  set(values: T['KeyType'][]): Parameter<T> {
     return new Parameter(this.keyName, this.keyTag, values, this.units)
   }
 }
