4ad988b61fb5c562d90678719482b7739ec5c378
Pritam Kadam <phkadam2008@gmail.com>
2020-07-10 16:57:30 +0530

    Use cached param decoders

--- lib/test/io-ts/key.test.ts
+++ lib/test/io-ts/key.test.ts
@@ -1,61 +1,60 @@
 import * as E from 'fp-ts/lib/Either'
-import { Either, getOrElse } from 'fp-ts/lib/Either'
 import { pipe } from 'fp-ts/lib/function'
 import { DecodeError } from 'io-ts/lib/DecodeError'
 import * as D from 'io-ts/lib/Decoder'
 import { FreeSemigroup } from 'io-ts/lib/FreeSemigroup'
 import { Parameter, UnitsDec } from '../../src/models'
-import { Key, Struct } from './../../src/models/params/Key'
+import {
+  IntArrayKey,
+  IntKey,
+  Key,
+  StringKey,
+  Struct,
+  StructKey
+} from './../../src/models/params/Key'
 
 type ErrorD = FreeSemigroup<DecodeError<string>>
 
-const get = <A>(result: Either<ErrorD, A>): A =>
-  getOrElse<ErrorD, A>((err) => {
+const get = <A>(result: E.Either<ErrorD, A>): A =>
+  E.getOrElse<ErrorD, A>((err) => {
     throw Error('Right value not present, Error: ' + D.draw(err))
   })(result)
 
 const KeyTagDecoder = D.literal('IntKey', 'StringKey', 'IntArrayKey', 'StructKey')
 
-const ParamsDecFactory = <T extends Key>(kt: T['KeyTag'], valuesDec: D.Decoder<T['KeyType']>) =>
+const ParameterDecoder: D.Decoder<Parameter<Key>> = pipe(
+  D.UnknownRecord,
+  D.parse((rec) => {
+    const key = Object.keys(rec)[0]
+    const body = rec[key]
+    return pipe(
+      KeyTagDecoder.decode(key),
+      E.chain((keyTag) =>
+        pipe(
+          ParamDecodersMap[keyTag].decode(body),
+          E.map((b) => new Parameter(b.keyName, keyTag, b.values, b.units))
+        )
+      )
+    )
+  })
+)
+
+const ParamBodyDecoder = <T extends Key>(valuesDec: D.Decoder<T['KeyType']>) =>
   D.type({
-    [kt]: D.type({
-      keyName: D.string,
-      values: D.array(valuesDec),
-      units: UnitsDec
-    })
+    keyName: D.string,
+    values: D.array(valuesDec),
+    units: UnitsDec
   })
 
-const ParameterDecoder: D.Decoder<Parameter<Key>> = {
-  decode: (input: unknown) =>
-    pipe(
-      ParamsDecoder.decode(input),
-      E.chain((result) => {
-        return pipe(
-          KeyTagDecoder.decode(Object.keys(result)[0]),
-          E.map((keyTag) => {
-            const { keyName, values, units } = result[keyTag]
-            return new Parameter(keyName, keyTag, values, units)
-          })
-        )
-      })
-    )
-}
-
-const IntParamDec = ParamsDecFactory('IntKey', D.number)
-const IntArrayKeyParamDec = ParamsDecFactory('IntArrayKey', D.array(D.number))
-const StringParamDec = ParamsDecFactory('StringKey', D.string)
-
 const StructDecoder: D.Decoder<Struct> = D.type({ paramSet: D.array(ParameterDecoder) })
-const StructParamDecoder = ParamsDecFactory('StructKey', StructDecoder)
-
-const ParamsDecoder = D.union(IntParamDec, StringParamDec, IntArrayKeyParamDec, StructParamDecoder)
-
-const parseParameter = (input: unknown) => get(ParameterDecoder.decode(input))
-const ParamSetDec = D.type({ paramSet: D.array(ParamsDecoder) })
-const parseParamSet = (input: unknown) => {
-  const rawParamSet = get(ParamSetDec.decode(input)).paramSet
-  return rawParamSet.map((p) => parseParameter(p))
+const ParamDecodersMap = {
+  IntKey: ParamBodyDecoder<IntKey>(D.number),
+  StringKey: ParamBodyDecoder<StringKey>(D.string),
+  IntArrayKey: ParamBodyDecoder<IntArrayKey>(D.array(D.number)),
+  StructKey: ParamBodyDecoder<StructKey>(StructDecoder)
 }
+
+const ParamSetDec = D.type({ paramSet: D.array(ParameterDecoder) })
 
 test('Parameter', () => {
   // input json to be decoded to Parameter class
@@ -78,8 +77,8 @@ test('Parameter', () => {
     }
   }
 
-  console.log(parseParameter(intParam))
-  console.log(parseParameter(intArrayRaw))
+  console.log(get(ParameterDecoder.decode(intParam)))
+  console.log(get(ParameterDecoder.decode(intArrayRaw)))
 })
 
 test('ParamSet', () => {
@@ -106,7 +105,7 @@ test('ParamSet', () => {
     ]
   }
 
-  console.log(parseParamSet(paramSet))
+  console.log(get(ParamSetDec.decode(paramSet)))
 })
 
 test('Struct', () => {
@@ -137,5 +136,5 @@ test('Struct', () => {
     }
   }
 
-  console.log(JSON.stringify(parseParameter(raw)))
+  console.log(JSON.stringify(get(ParameterDecoder.decode(raw))))
 })
