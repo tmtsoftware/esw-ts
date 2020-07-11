896090e5b5c7c8716860f5a72638dc18118e5e62
Pritam Kadam <phkadam2008@gmail.com>
2020-07-11 14:49:32 +0530

    Use helper KTag and KType to extract key tag and key type from Key

--- lib/src/models/params/BaseKey.ts
+++ lib/src/models/params/BaseKey.ts
@@ -1,11 +1,11 @@
-import { Key } from './Key'
+import { Key, KTag, KType } from './Key'
 import { Parameter } from './Parameter'
 import { Units } from './Units'
 
 export class BaseKey<T extends Key> {
-  constructor(readonly keyName: string, readonly keyTag: T['KeyTag'], readonly units: Units) {}
+  constructor(readonly keyName: string, readonly keyTag: KTag<T>, readonly units: Units) {}
 
-  set(values: T['KeyType'][]): Parameter<T> {
+  set(values: KType<T>[]): Parameter<T> {
     return new Parameter(this.keyName, this.keyTag, values, this.units)
   }
 }
