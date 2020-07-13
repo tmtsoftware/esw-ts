0393d64bc1e52c98244a2cfa48f5dfa061a55a2e
Shubham Chauhan <shubham25may1998@gmail.com>
2020-07-13 20:14:59 +0530

    #000: Chauhan : Fix PrefixV.is function

--- lib/src/models/params/Prefix.ts
+++ lib/src/models/params/Prefix.ts
@@ -14,15 +14,20 @@ const parseSubsystemStr = (subsystem: string): Subsystem => {
   else throw Error(`Subsystem: ${subsystem} is invalid`)
 }
 
+const splitSubsystemComponentName = (prefixStr: string) => {
+  const stringL = prefixStr.split(SEPARATOR)
+  const sub = stringL[0]
+  const componentName = stringL.slice(1).join(SEPARATOR)
+  return [sub, componentName]
+}
+
 export class Prefix {
   constructor(readonly subsystem: Subsystem, readonly componentName: string) {
     validateComponentName(componentName)
   }
 
   static fromString = (prefixStr: string): Prefix => {
-    const stringL = prefixStr.split(SEPARATOR)
-    const sub = stringL[0]
-    const componentName = stringL.slice(1).join(SEPARATOR)
+    const [sub, componentName] = splitSubsystemComponentName(prefixStr)
     validateComponentName(componentName)
     return new Prefix(parseSubsystemStr(sub), componentName)
   }
@@ -39,7 +44,15 @@ const decodePrefix = (input: unknown, context: t.Context): t.Validation<Prefix> 
 
 const encodePrefix = (prefix: Prefix) => prefix.subsystem + SEPARATOR + prefix.componentName
 
-const isPrefix = (input: unknown): input is Prefix => input instanceof Prefix
+const isPrefix = (input: unknown): input is Prefix => {
+  if (t.string.is(input)) {
+    const [sub, componentName] = splitSubsystemComponentName(input)
+    return (
+      Subsystem.is(sub) && componentName === componentName.trim() && !componentName.includes('-')
+    )
+  }
+  return false
+}
 
 export const PrefixV: t.Type<Prefix, string> = new t.Type(
   'Prefix',
