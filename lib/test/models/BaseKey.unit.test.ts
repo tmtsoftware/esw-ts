2f45be25d003e2d1d65248819b122e20aeb37a68
ManishGowardipe <gowardipemanish95@gmail.com>
2020-08-14 17:53:47 +0530

    #ESW-320 :  Manish | Himanshu : Add base key unit test

---
+++ lib/test/models/BaseKey.unit.test.ts
@@ -0,0 +1,16 @@
+import { BaseKey, Key, Parameter } from '../../src/models'
+
+describe('Base Key', () => {
+  test('should return parameter on seting values', () => {
+    const key: BaseKey<Key> = new BaseKey('prime numbers', 'IntKey', 'NoUnits')
+
+    const keyParameter: Parameter<Key> = key.set([1, 2, 3])
+
+    expect(keyParameter).toEqual({
+      keyName: 'prime numbers',
+      keyTag: 'IntKey',
+      units: 'NoUnits',
+      values: [1, 2, 3]
+    })
+  })
+})
