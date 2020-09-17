660b4c0e2619cbfb49ac60300712b9c9981fb19d
ManishGowardipe <gowardipemanish95@gmail.com>
2020-09-17 13:00:10 +0530

    #000: Vinay | Manish  : Update alarm post test to use verify helper

--- lib/test/clients/alarm/AlarmPostService.unit.test.ts
+++ lib/test/clients/alarm/AlarmPostService.unit.test.ts
@@ -3,20 +3,28 @@ import { AlarmServiceImpl } from '../../../src/clients/alarm/AlarmServiceImpl'
 import { SetAlarmSeverity } from '../../../src/clients/alarm/models/PostCommand'
 import { DoneD } from '../../../src/clients/location'
 import { Prefix } from '../../../src/models'
-import { mockHttpTransport } from '../../helpers/MockHelpers'
+import { verify } from '../../helpers/JestMockHelpers'
+import { HttpTransport } from '../../../src/utils/HttpTransport'
+import { mocked } from 'ts-jest/utils'
 
-const mockResponse = Math.random().toString()
-const requestRes: jest.Mock = jest.fn().mockReturnValue(Promise.resolve(mockResponse))
-const alarmService = new AlarmServiceImpl(mockHttpTransport(requestRes))
+jest.mock('../../../src/utils/HttpTransport')
+
+const httpTransport: HttpTransport<SetAlarmSeverity> = new HttpTransport('')
+const alarmService = new AlarmServiceImpl(httpTransport)
+const mockedHttpTransport = mocked(httpTransport)
 
 describe('Alarm service', () => {
   test('should set alarm severity for a given prefix | ESW-314', async () => {
     const alarmKey = new AlarmKey(new Prefix('ESW', 'Comp1'), 'alarm1')
     const severity = 'Okay'
 
+    mockedHttpTransport.requestRes.mockResolvedValueOnce('Done')
     const response = await alarmService.setSeverity(alarmKey, severity)
 
-    expect(response).toEqual(mockResponse)
-    expect(requestRes).toBeCalledWith(new SetAlarmSeverity(alarmKey, severity), DoneD)
+    expect(response).toEqual('Done')
+    verify(mockedHttpTransport.requestRes).toBeCalledWith(
+      new SetAlarmSeverity(alarmKey, severity),
+      DoneD
+    )
   })
 })
