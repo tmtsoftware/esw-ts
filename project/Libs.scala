52e5ed26641822884a67860dbdc5f07ed2ac39c9
mausamyede <mausam.yede1@gmail.com>
2020-09-18 16:03:15 +0530

    #ESW-382: Mausam : Upgrade dependency versions

--- project/Libs.scala
+++ project/Libs.scala
@@ -3,12 +3,12 @@ import sbt._
 object Libs {
   val ScalaVersion = "2.13.3"
 
-  val `scalaTest`         = "org.scalatest"                            %% "scalatest"         % "3.1.2" //Apache License 2.0
+  val `scalaTest`         = "org.scalatest"                            %% "scalatest"         % "3.2.2" //Apache License 2.0
   val `selenium-java`     = "org.seleniumhq.selenium"                   % "selenium-java"     % "3.141.59"
   val `embedded-keycloak` = "com.github.tmtsoftware.embedded-keycloak" %% "embedded-keycloak" % "7fd5623"
-  val `webdriverManager`  = "io.github.bonigarcia"                      % "webdrivermanager"  % "4.1.0"
+  val `webdriverManager`  = "io.github.bonigarcia"                      % "webdrivermanager"  % "4.2.0"
   val `tmt-test-reporter` = "com.github.tmtsoftware"                   %% "rtm"               % "05e9789f460"
-  val scalaTestPlus       = "org.scalatestplus"                        %% "selenium-3-141"    % "3.1.2.0"
+  val scalaTestPlus       = "org.scalatestplus"                        %% "selenium-3-141"    % "3.2.2.0"
 }
 
 object CSW {
