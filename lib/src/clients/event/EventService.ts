a238dc701b4b0397a498bad921849bd5ea0ec3f2
ManishGowardipe <gowardipemanish95@gmail.com>
2020-10-01 16:12:03 +0530

    #ESW-401: Manish: Add Api documentation for event service

--- lib/src/clients/event/EventService.ts
+++ lib/src/clients/event/EventService.ts
@@ -1,7 +1,7 @@
 import { gatewayConnection, resolveConnection } from '../../config/Connections'
 import type { Subsystem } from '../../models'
 import { HttpTransport } from '../../utils/HttpTransport'
-import type { Subscription } from '../../utils/types'
+import type { Subscription } from '../..'
 import { getPostEndPoint, getWebSocketEndPoint } from '../../utils/Utils'
 import { Ws } from '../../utils/Ws'
 import type { Done } from '../location'
@@ -9,16 +9,44 @@ import { EventServiceImpl } from './EventServiceImpl'
 import type { Event } from './models/Event'
 import type { EventKey } from './models/EventKey'
 
+/**
+ * A Event Service API provides client side methods to interact with the event server.
+ * @interface
+ */
 export interface EventService {
+  /**
+   * Publishes an event
+   * @param event the event to be published
+   * @return Done as a promise
+   */
   publish(event: Event): Promise<Done>
 
+  /**
+   * Gets the list of event for the given set of event keys
+   * @param eventKeys Set of event keys against which event are fetched.
+   * @return Event[] as a promise
+   */
   get(eventKeys: Set<EventKey>): Promise<Event[]>
-
+  /**
+   * Subscribes to given set of event keys
+   * @param eventKeys Set of event keys to be subscribed.
+   * @param maxFrequency the duration which determines the frequency with which events are received
+   * @param callback the function which gets triggered on receiving an event
+   * @return Subscription
+   */
   subscribe(
     eventKeys: Set<EventKey>,
     maxFrequency: number
   ): (callback: (event: Event) => void) => Subscription
-
+  /**
+   * Subscribes to given subsystem and optional pattern string.
+   *
+   * @param subsystem subscription to be made under the subsystem
+   * @param maxFrequency the duration which determines the frequency with which events are received
+   * @param pattern optional pattern to match against the event key
+   * @param callback the function which gets triggered on receiving an event
+   * @return Subscription
+   */
   pSubscribe(
     subsystem: Subsystem,
     maxFrequency: number,
@@ -26,6 +54,10 @@ export interface EventService {
   ): (callback: (event: Event) => void) => Subscription
 }
 
+/**
+ * Instantiate Event service to enable interaction with the event server.
+ * @constructor
+ */
 export const EventService = async (): Promise<EventService> => {
   const { host, port } = await resolveConnection(gatewayConnection)
   const postEndpoint = getPostEndPoint({ host, port })
