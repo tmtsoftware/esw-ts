093c09e243b2bde31f49c0ec4b1d3fab063eff82
Shivani Thorat <shivanimt31@gmail.com>
2020-11-05 16:26:49 +0530

    #ESW-383 : Shivani : Add coursier set-up to ci and update dependencies for release

--- project/Libs.scala
+++ project/Libs.scala
@@ -3,7 +3,7 @@ import sbt._
 object Libs {
   val ScalaVersion = "2.13.3"
 
-  val `scalaTest`         = "org.scalatest"                            %% "scalatest"         % "3.2.2" //Apache License 2.0
+  val `scalaTest`         = "org.scalatest"                            %% "scalatest"         % "3.1.4" //Apache License 2.0
   val `selenium-java`     = "org.seleniumhq.selenium"                   % "selenium-java"     % "3.141.59"
   val `embedded-keycloak` = "com.github.tmtsoftware.embedded-keycloak" %% "embedded-keycloak" % "0.2.0-M1"
   val `webdriverManager`  = "io.github.bonigarcia"                      % "webdrivermanager"  % "4.2.2"
