1166ac2b4d026bdd5d671c0308fb0b39f45ed291
Poorva Gokhale <poorvasgokhale@gmail.com>
2020-12-21 12:04:07 +0530

    #ESW-418: Manish | Poorva: Setup web test runner with snowpack

---
+++ lib/test/sample.unit.test.ts
@@ -0,0 +1,10 @@
+import {expect} from '@esm-bundle/chai'
+
+export function sum(a: number, b: number) {
+  return a + b;
+}
+
+it('sums up 2 numbers', () => {
+  expect(sum(1, 1)).to.equal(2);
+  expect(sum(3, 12)).to.equal(15);
+});
