0ac41018972f6e40d8e384d61b7a6cda62924ac1
Vinay Mahamuni <vinaymahamuni22@gmail.com>
2021-03-08 16:14:22 +0530

    #ESW-475: Vinay | Pritam | Manish: Add ability to switch between production and local locationConfig
    currently dummy production hostname and port of location service is used. when we go live for first time, this should be changed with actual values

--- lib/src/config/LocationConfig.ts
+++ lib/src/config/LocationConfig.ts
@@ -1,4 +1,6 @@
-export const LocationConfig = {
-  hostName: 'localhost',
-  port: 7654
+const isRunningInProduction = process.env.NODE_ENV === 'production'
+
+export const LocationConfig: { hostName: string; port: number } = {
+  hostName: isRunningInProduction ? 'production-url-of-location-service.com' : 'localhost',
+  port: isRunningInProduction ? 8765 : 7654
 }
---
+++ lib/test/config/LocationConfig.unit.test.ts
@@ -0,0 +1,28 @@
+describe('LocationConfig', () => {
+  const OLD_ENV = process.env
+
+  beforeEach(() => {
+    jest.resetModules() //it clears the cache
+    process.env = { ...OLD_ENV }
+  })
+
+  afterAll(() => {
+    process.env = OLD_ENV
+  })
+
+  test('should give production hostname and port when NODE_ENV is set to production', async () => {
+    process.env.NODE_ENV = 'production'
+    const { LocationConfig } = await import('../../src/config/LocationConfig')
+
+    expect(LocationConfig.hostName).toEqual('production-url-of-location-service.com')
+    expect(LocationConfig.port).toEqual(8765)
+  })
+
+  test('should give local hostname and port when NODE_ENV is not set to production', async () => {
+    process.env.NODE_ENV = 'dev'
+    const { LocationConfig } = await import('../../src/config/LocationConfig')
+
+    expect(LocationConfig.hostName).toEqual('localhost')
+    expect(LocationConfig.port).toEqual(7654)
+  })
+})
