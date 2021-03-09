25193ca49cda5ef8f5ac886052536e975bb3d713
Vinay Mahamuni <vinaymahamuni22@gmail.com>
2021-03-09 12:43:08 +0530

    #ESW-475: Vinay: Update dummy production hostname

--- lib/src/config/LocationConfig.ts
+++ lib/src/config/LocationConfig.ts
@@ -1,6 +1,6 @@
 const isRunningInProduction = process.env.NODE_ENV === 'production'
 
 export const LocationConfig: { hostName: string; port: number } = {
-  hostName: isRunningInProduction ? 'production-url-of-location-service.com' : 'localhost',
+  hostName: isRunningInProduction ? 'production-dns-entry.com' : 'localhost',
   port: isRunningInProduction ? 8765 : 7654
 }
