17e289a2bc595aa7bca3b105e9269dcb7b0af042
Vinay Mahamuni <vinaymahamuni22@gmail.com>
2021-05-28 18:41:10 +0530

    #ESW-447: Vinay: Add and export list of subsystems

--- lib/src/models/params/Subsystem.ts
+++ lib/src/models/params/Subsystem.ts
@@ -1,34 +1,37 @@
+export const subsystems = [
+  'AOESW',
+  'APS',
+  'CIS',
+  'CLN',
+  'CRYO',
+  'CSW',
+  'DMS',
+  'DPS',
+  'ESEN',
+  'ESW',
+  'FMCS',
+  'GMS',
+  'IRIS',
+  'LGSF',
+  'M1CS',
+  'MODHIS',
+  'NFIRAOS',
+  'NSCU',
+  'OSS',
+  'PFCS',
+  'PSFR',
+  'REFR',
+  'RTC',
+  'RPG',
+  'SCMS',
+  'SOSS',
+  'TCS',
+  'WFOS',
+  'Container'
+] as const
+
 /**
  * A union type representing TMT Subsystems
  * @category Params
  */
-export type Subsystem =
-  | 'AOESW'
-  | 'APS'
-  | 'CIS'
-  | 'CLN'
-  | 'CRYO'
-  | 'CSW'
-  | 'DMS'
-  | 'DPS'
-  | 'ESEN'
-  | 'ESW'
-  | 'FMCS'
-  | 'GMS'
-  | 'IRIS'
-  | 'LGSF'
-  | 'M1CS'
-  | 'MODHIS'
-  | 'NFIRAOS'
-  | 'NSCU'
-  | 'OSS'
-  | 'PFCS'
-  | 'PSFR'
-  | 'REFR'
-  | 'RTC'
-  | 'RPG'
-  | 'SCMS'
-  | 'SOSS'
-  | 'TCS'
-  | 'WFOS'
-  | 'Container'
+export type Subsystem = typeof subsystems[number]
