7de24dd7b342afce7414180621d6586997302b34
Himanshu Mertia <22441818+mertia-himanshu@users.noreply.github.com>
2021-06-30 18:34:37 +0530

    #ESW-000 : Himanshu : Use 'test' semantics instead of 'it' in tests

--- test/clients/sequencer/model/StepList.unit.test.ts
+++ test/clients/sequencer/model/StepList.unit.test.ts
@@ -30,38 +30,38 @@ const failureStep: Step = {
   status: { _type: 'Failure', message: 'failure message' }
 }
 describe('StepList toJSON', () => {
-  it('should return object to be converted into JSON | ESW-454', () => {
+  test('should return object to be converted into JSON | ESW-454', () => {
     const stepList = new StepList([successStep])
 
     expect(stepList.toJSON()).toEqual([successStep])
   })
 })
 describe('StepList isPaused', () => {
-  it('should return true if first step is pending and has breakpoint | ESW-454', () => {
+  test('should return true if first step is pending and has breakpoint | ESW-454', () => {
     const stepList = new StepList([pendingStepWithBreakpoint])
 
     expect(stepList.isPaused()).toBeTruthy()
   })
 
-  it('should return true if first step is successful and second step is pending and has breakpoint | ESW-454', () => {
+  test('should return true if first step is successful and second step is pending and has breakpoint | ESW-454', () => {
     const stepList = new StepList([successStep, pendingStepWithBreakpoint])
 
     expect(stepList.isPaused()).toBeTruthy()
   })
 
-  it("should return false if first step is successful and second step is pending but doesn't have breakpoint | ESW-454", () => {
+  test("should return false if first step is successful and second step is pending but doesn't have breakpoint | ESW-454", () => {
     const stepList = new StepList([successStep, pendingStepWithoutBreakpoint])
 
     expect(stepList.isPaused()).toBeFalsy()
   })
 
-  it('should return false if no step is Pending | ESW-454', () => {
+  test('should return false if no step is Pending | ESW-454', () => {
     const stepList = new StepList([successStep, failureStep])
 
     expect(stepList.isPaused()).toBeFalsy()
   })
 
-  it('should return false if stepList is empty | ESW-454', () => {
+  test('should return false if stepList is empty | ESW-454', () => {
     const stepList = new StepList([])
 
     expect(stepList.isPaused()).toBeFalsy()
@@ -69,19 +69,19 @@ describe('StepList isPaused', () => {
 })
 
 describe('StepList isFailed', () => {
-  it('should return true if any step is Failed | ESW-454', () => {
+  test('should return true if any step is Failed | ESW-454', () => {
     const stepList = new StepList([successStep, failureStep])
 
     expect(stepList.isFailed()).toBeTruthy()
   })
 
-  it('should return false if no steps are Failed | ESW-454', () => {
+  test('should return false if no steps are Failed | ESW-454', () => {
     const stepList = new StepList([successStep, pendingStepWithBreakpoint])
 
     expect(stepList.isFailed()).toBeFalsy()
   })
 
-  it('should return false if stepList is empty | ESW-454', () => {
+  test('should return false if stepList is empty | ESW-454', () => {
     const stepList = new StepList([])
 
     expect(stepList.isFailed()).toBeFalsy()
