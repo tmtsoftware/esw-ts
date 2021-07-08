b17d8c9653a811c270b5066d9411e64c95e50dd4
anujachaitanya <anujachaitanya09@gmail.com>
2021-07-08 12:41:26 +0530

    #000 : Anuja | Manish : Export keyCloack type and client Impls under testUtils

---
+++ src/utils/TestUtils.ts
@@ -0,0 +1,11 @@
+export type {
+  KeycloakProfile,
+  KeycloakPromise,
+  KeycloakResourceAccess,
+  KeycloakRoles,
+  KeycloakTokenParsed
+} from 'keycloak-js'
+export { AgentServiceImpl } from '../clients/agent-service/AgentServiceImpl'
+export { ConfigServiceImpl } from '../clients/config-service/ConfigServiceImpl'
+export { SequenceManagerImpl } from '../clients/sequence-manager/SequenceManagerImpl'
+export { SequencerServiceImpl } from '../clients/sequencer/SequencerServiceImpl'
