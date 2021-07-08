749a3dc75091b6d4202a3767563a678c4a1abf34
Shubham Chauhan <shubham25may1998@gmail.com>
2021-07-08 13:34:29 +0530

    #ESW-519: Chauhan: Update csw and esw version for tests

--- integration/utils/shell.ts
+++ integration/utils/shell.ts
@@ -35,8 +35,8 @@ const appLauncherSync = (name: string, version = 'master-SNAPSHOT') =>
 
 // Put SHA of 10 characters to make it consistent with Git api for latest SHA
 // This consistency avoids multiple jitpack triggers for same commit
-const csw_sha = '33131de840'
-const esw_sha = '2d7e0e2eae'
+const csw_sha = '4.0.0-M1'
+const esw_sha = '0.3.0-M1'
 
 export const executeServicesScript = appLauncher('backend-testkit-services', esw_sha)
 export const executeComponentScript = appLauncher('backend-testkit-component', esw_sha)
