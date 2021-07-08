af7f323b5c6c9aeaaa870c39543170e56e52a16c
Shubham Chauhan <shubham25may1998@gmail.com>
2021-07-08 12:58:15 +0530

    #ESW-519: Chauhan: Update sbt and scala dependencies

--- project/Libs.scala
+++ project/Libs.scala
@@ -1,14 +1,14 @@
 import sbt._
 
 object Libs {
-  val ScalaVersion = "2.13.3"
+  val ScalaVersion = "2.13.5"
 
-  val `scalaTest`         = "org.scalatest"                            %% "scalatest"         % "3.1.4" //Apache License 2.0
+  val `scalaTest`         = "org.scalatest"                            %% "scalatest"         % "3.2.9" //Apache License 2.0
   val `selenium-java`     = "org.seleniumhq.selenium"                   % "selenium-java"     % "3.141.59"
-  val `embedded-keycloak` = "com.github.tmtsoftware.embedded-keycloak" %% "embedded-keycloak" % "0.2.0"
-  val `webdriverManager`  = "io.github.bonigarcia"                      % "webdrivermanager"  % "4.2.2"
-  val `tmt-test-reporter` = "com.github.tmtsoftware"                   %% "rtm"               % "fecc848abf7"
-  val scalaTestPlus       = "org.scalatestplus"                        %% "selenium-3-141"    % "3.2.2.0"
+  val `embedded-keycloak` = "com.github.tmtsoftware.embedded-keycloak" %% "embedded-keycloak" % "0.4.0"
+  val `webdriverManager`  = "io.github.bonigarcia"                      % "webdrivermanager"  % "4.4.3"
+  val `tmt-test-reporter` = "com.github.tmtsoftware"                   %% "rtm"               % "0.1.0"
+  val scalaTestPlus       = "org.scalatestplus"                        %% "selenium-3-141"    % "3.2.9.0"
 }
 
 object ESW {
--- project/plugins.sbt
+++ project/plugins.sbt
@@ -1,4 +1,4 @@
-addSbtPlugin("com.timushev.sbt" % "sbt-updates"        % "0.5.1")
+addSbtPlugin("com.timushev.sbt" % "sbt-updates"        % "0.5.3")
 addSbtPlugin("org.scalameta"    % "sbt-scalafmt"       % "2.4.2")
 addSbtPlugin("com.dwijnand"     % "sbt-dynver"         % "4.1.1")
 addSbtPlugin("io.spray"         % "sbt-revolver"       % "0.9.1")
@@ -7,6 +7,6 @@ addSbtPlugin("ohnosequences"    % "sbt-github-release" % "0.7.0")
 libraryDependencies += "com.sun.activation" % "javax.activation" % "1.2.0"
 
 resolvers += "jitpack" at "https://jitpack.io"
-libraryDependencies += "com.github.tmtsoftware" % "sbt-docs" % "9fe4596ff8"
+libraryDependencies += "com.github.tmtsoftware" % "sbt-docs" % "0.3.0"
 
 classpathTypes += "maven-plugin"
