caf8020aa33813a4dd71a838d7f82a8dcc57ae6e
Manish <manish.gowardipe@thoughtworks.com>
2021-07-23 16:29:05 +0530

    #CSW-100: Manish : Update subsystem list

--- integration/utils/shell.ts
+++ integration/utils/shell.ts
@@ -35,8 +35,8 @@ const appLauncherSync = (name: string, version = 'master-SNAPSHOT') =>
 
 // Put SHA of 10 characters to make it consistent with Git api for latest SHA
 // This consistency avoids multiple jitpack triggers for same commit
-const csw_sha = '00e30a1946'
-const esw_sha = '575e2007d2'
+const csw_sha = '902746e2a1'
+const esw_sha = '827bbf48e'
 
 export const executeServicesScript = appLauncher('backend-testkit-services', esw_sha)
 export const executeComponentScript = appLauncher('backend-testkit-component', esw_sha)
