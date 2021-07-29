14c303381c44da059f03f7fae6848d8658f27911
Shubham Chauhan <shubham25may1998@gmail.com>
2021-07-29 10:14:22 +0530

    #ESW-537: Chauhan: Create a Units map which have units as keys and their name as values

--- src/models/params/Units.ts
+++ src/models/params/Units.ts
@@ -1,68 +1,71 @@
+export const Units = {
+  // SI units
+  angstrom: 'Angstrom',
+  arcmin: 'arcmin',
+  arcsec: 'arcsec',
+  day: 'd',
+  degree: 'deg',
+  elvolt: 'eV',
+  gram: 'g',
+  hour: 'h',
+  hertz: 'Hz',
+  joule: 'J',
+  kelvin: 'K',
+  kilogram: 'kg',
+  kilometer: 'km',
+  liter: 'l',
+  meter: 'm',
+  marcsec: 'mas',
+  millimeter: 'mm',
+  millisecond: 'ms',
+  micron: 'µm',
+  micrometer: 'µm',
+  minute: 'min',
+  newton: 'N',
+  pascal: 'Pa',
+  radian: 'rad',
+  second: 's',
+  sday: 'sday',
+  steradian: 'sr',
+  microarcsec: 'µas',
+  volt: 'V',
+  watt: 'W',
+  week: 'wk',
+  year: 'yr',
+
+  // CGS unit
+  coulomb: 'C',
+  centimeter: 'cm',
+  erg: 'erg',
+
+  // Astropyhsics units
+  au: 'AU',
+  jansky: 'Jy',
+  lightyear: 'lyr',
+  mag: 'mag',
+
+  // Imperial units
+  cal: 'cal',
+  foot: 'ft',
+  inch: 'inch',
+  pound: 'lb',
+  mile: 'mi',
+  ounce: 'oz',
+  yard: 'yd',
+
+  // Others - engineering
+  NoUnits: 'none',
+  encoder: 'enc',
+  count: 'ct',
+  pix: 'pix',
+
+  // Time units
+  tai: 'TAI',
+  utc: 'UTC'
+}
+
 /**
  * A union type representing units for TMT
  * @category Params
  */
-export type Units =
-  // SI units
-  | 'angstrom'
-  | 'arcmin'
-  | 'arcsec'
-  | 'day'
-  | 'degree'
-  | 'elvolt'
-  | 'gram'
-  | 'hour'
-  | 'hertz'
-  | 'joule'
-  | 'kelvin'
-  | 'kilogram'
-  | 'kilometer'
-  | 'liter'
-  | 'meter'
-  | 'marcsec'
-  | 'millimeter'
-  | 'millisecond'
-  | 'micron'
-  | 'micrometer'
-  | 'minute'
-  | 'newton'
-  | 'pascal'
-  | 'radian'
-  | 'second'
-  | 'sday'
-  | 'steradian'
-  | 'microarcsec'
-  | 'volt'
-  | 'watt'
-  | 'week'
-  | 'year'
-
-  // CGS unit
-  | 'coulomb'
-  | 'centimeter'
-  | 'erg'
-
-  // Astropyhsics units
-  | 'au'
-  | 'jansky'
-  | 'lightyear'
-  | 'mag'
-
-  // Imperial units
-  | 'cal'
-  | 'foot'
-  | 'inch'
-  | 'pound'
-  | 'mile'
-  | 'ounce'
-  | 'yard'
-
-  // Others - engineering
-  | 'NoUnits'
-  | 'encoder'
-  | 'count'
-  | 'pix'
-
-  // Time units
-  | 'tai'
-  | 'utc'
+export type Units = keyof typeof Units
