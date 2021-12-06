0fa9d39d7d53fa36afbefc8eddea477129ca3ccc
manish <gowardipemanish95@gmail.com>
2021-12-06 17:20:07 +0530

    #ESW-565 :  Manish | Chauhan : update esw sha's

--- integration/utils/shell.ts
+++ integration/utils/shell.ts
@@ -36,7 +36,7 @@ const appLauncherSync = (name: string, version = 'master-SNAPSHOT') =>
 // Put SHA of 10 characters to make it consistent with Git api for latest SHA
 // This consistency avoids multiple jitpack triggers for same commit
 const csw_sha = '4.0.0'
-const esw_sha = '94453e0'
+const esw_sha = 'cd601b9'
 
 export const executeServicesScript = appLauncher('backend-testkit-services', esw_sha)
 export const executeComponentScript = appLauncher('backend-testkit-component', esw_sha)
--- project/Libs.scala
+++ project/Libs.scala
@@ -12,7 +12,7 @@ object Libs {
 }
 
 object ESW {
-  private val defaultESWVersion = "94453e0"
+  private val defaultESWVersion = "cd601b9"
 
   val Version: Option[String] = sys.props.get("prod.publish").collect {
     case "true" =>
