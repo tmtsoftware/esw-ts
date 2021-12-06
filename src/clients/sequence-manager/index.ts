d5b67b6486004b1094607e3c0f983278cb82b581
manish <gowardipemanish95@gmail.com>
2021-12-06 16:48:29 +0530

    #ESW-565 :  Manish | Chauhan : Export variationInfo from top level models

--- src/clients/sequence-manager/index.ts
+++ src/clients/sequence-manager/index.ts
@@ -1,5 +1,6 @@
 export * from './models/ObsMode'
 export * from './models/Variation'
+export * from './models/VariationInfo'
 export * from './models/ProvisionConfig'
 export * from './models/SequenceManagerRes'
 export * from './SequenceManagerService'
