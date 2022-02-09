4ddea9fa45e2e8ba0d6b04efab3fbeebb26ac9e4
manish <gowardipemanish95@gmail.com>
2022-02-09 17:48:52 +0530

    #ESW-000: Manish | Himanshu : Use latest csw sha for nanometer changes

--- integration/utils/shell.ts
+++ integration/utils/shell.ts
@@ -36,7 +36,7 @@ const appLauncherSync = (name: string, version = 'master-SNAPSHOT') =>
 // Put SHA of 10 characters to make it consistent with Git api for latest SHA
 // This consistency avoids multiple jitpack triggers for same commit
 
-const csw_sha = '67ece56'
+const csw_sha = 'd05f7d2'
 const esw_sha = '91cc37a'
 
 export const executeServicesScript = appLauncher('backend-testkit-services', esw_sha)
