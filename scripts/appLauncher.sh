8e91db9d8b37e8e1ad3a052c69252fb69438d533
Denis Zaytsev <dzaytsev@tmt.org>
2022-02-24 12:50:55 -0800

    nightly-build pipeline fix, add eval

--- scripts/appLauncher.sh
+++ scripts/appLauncher.sh
@@ -6,6 +6,7 @@ APP_NAME="$1"
 APP_VERSION="$2"
 
 cs java --jvm openjdk:1.17.0
+eval $(cs java --jvm openjdk:1.17.0 --env -m offline)
 
 # Pass rest of the arguments after first argument to the corresponding app
 cs launch --channel $APPS_PATH "$APP_NAME":"$APP_VERSION" -- "${@:3}"
