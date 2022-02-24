bd0035a9d5b5cc8ddfda155472ee8f742a4b9905
Denis Zaytsev <dzaytsev@tmt.org>
2022-02-24 12:13:26 -0800

    nightly-build pipeline fix (temurin)

--- scripts/appLauncher.sh
+++ scripts/appLauncher.sh
@@ -5,7 +5,7 @@ APPS_PATH="https://raw.githubusercontent.com/tmtsoftware/osw-apps/master/apps.js
 APP_NAME="$1"
 APP_VERSION="$2"
 
-eval `cs java --jvm 17`
+eval `cs java --jvm temurin:1.17`
 
 # Pass rest of the arguments after first argument to the corresponding app
 cs launch --channel $APPS_PATH "$APP_NAME":"$APP_VERSION" -- "${@:3}"
