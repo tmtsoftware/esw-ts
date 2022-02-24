f7d626853946064fa9aa27f184f65bbc7d9a5278
Denis Zaytsev <dzaytsev@tmt.org>
2022-02-24 12:44:22 -0800

    nightly-build pipeline fix, another openjdk:1.17.0

--- scripts/appLauncher.sh
+++ scripts/appLauncher.sh
@@ -5,8 +5,7 @@ APPS_PATH="https://raw.githubusercontent.com/tmtsoftware/osw-apps/master/apps.js
 APP_NAME="$1"
 APP_VERSION="$2"
 
-cs java --available | grep 17
-cs java --jvm adoptium:1.17.0.2
+cs java --jvm openjdk:1.17.0
 
 # Pass rest of the arguments after first argument to the corresponding app
 cs launch --channel $APPS_PATH "$APP_NAME":"$APP_VERSION" -- "${@:3}"
