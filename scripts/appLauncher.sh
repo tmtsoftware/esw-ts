9ed769d5e205acc418611222d04996350e908565
Shubham Chauhan <shubham25may1998@gmail.com>
2022-03-07 17:44:23 +0530

    #000: Chauhan: Remove -m offline from jvm install command

--- scripts/appLauncher.sh
+++ scripts/appLauncher.sh
@@ -5,7 +5,7 @@ APPS_PATH="https://raw.githubusercontent.com/tmtsoftware/osw-apps/master/apps.js
 APP_NAME="$1"
 APP_VERSION="$2"
 
-eval $(cs java --jvm temurin:1.17.0 --env -m offline)
+eval $(cs java --jvm temurin:1.17.0 --env)
 
 # Pass rest of the arguments after first argument to the corresponding app
 cs launch --channel $APPS_PATH "$APP_NAME":"$APP_VERSION" -- "${@:3}"
