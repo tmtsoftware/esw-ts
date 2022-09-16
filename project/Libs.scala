674dcc95d9dc2c77b1f868070a2e9004841ee333
Denis Zaytsev <dzaytsev@tmt.org>
2022-09-16 12:21:54 -0700

    preparing for v0.4.0-RC1, fixes for CSW and ESW versions

--- project/Libs.scala
+++ project/Libs.scala
@@ -12,7 +12,7 @@ object Libs {
 }
 
 object ESW {
-  private val defaultESWVersion = "bc4c382183"
+  private val defaultESWVersion = "0.5.0-RC1"
 
   val Version: String = sys.props.get("prod.publish") match {
     case Some("true") => sys.env.getOrElse("ESW_VERSION", defaultESWVersion)
@@ -21,7 +21,7 @@ object ESW {
 }
 
 object CSW {
-  private val defaultCswVersion = "879ffaefd7"
+  private val defaultCswVersion = "5.0.0-RC1"
 
   val Version: String = sys.props.get("prod.publish") match {
     case Some("true") => sys.env.getOrElse("CSW_VERSION", defaultCswVersion)
