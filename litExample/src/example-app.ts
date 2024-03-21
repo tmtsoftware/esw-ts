7e692af18868d035f82a69dc41303228faeb1aa3
Allan Brighton <Allan.Brighton@t-online.de>
2024-03-21 21:02:19 +0100

    In progress Lit example app with shoelace UI (needs work)

--- litExample/src/example-app.ts
+++ litExample/src/example-app.ts
@@ -5,25 +5,15 @@ import { Task } from '@lit/task'
 import litLogo from './assets/lit.svg'
 import viteLogo from '/vite.svg'
 import '../../src/components/aas/context/AuthContextProviderLit.ts'
-import './tab-bar.ts'
+import '@shoelace-style/shoelace/dist/components/tab-group/tab-group.js'
+import '@shoelace-style/shoelace/dist/components/tab/tab.js'
+import '@shoelace-style/shoelace/dist/components/tab-panel/tab-panel.js'
 
 /**
  * This is the top level example application element
  */
 @customElement('example-app')
 export class ExampleApp extends LitElement {
-  @property()
-  docsHint = 'Click on the Vite and Lit logos to learn more'
-
-  @property({ type: Number })
-  count = 0
-
-  // XXX TODO FIXME
-  @property({ attribute: false, reflect: true })
-  auth: Auth | null = null
-
-  @property({ type: Boolean })
-  failed = false
 
   private initTask = new Task(this, {
     task: async () => {
@@ -34,115 +24,40 @@ export class ExampleApp extends LitElement {
 
   render() {
     return this.initTask.render({
-      initial: () => html`<p>Not running yet ...</p>`,
-      pending: () => html`<p>Loading ...</p>`,
-      error: (e) => html`<p>Error: ${e}</p>`,
+      initial: () => html`<p>Starting ...</p>`,
+      pending: () => html`<p>Loading global config ...</p>`,
+      error: (e) => html`<p>Error loading global config: ${e}</p>`,
       complete: () => this.renderMain()
     })
   }
 
   renderMain() {
     return html`
-      <tab-bar></tab-bar>
       <auth-context-provider realm="TMT" client-id="tmt-frontend-app">
-        <div>
-          <a href="https://vitejs.dev" target="_blank">
-            <img src=${viteLogo} class="logo" alt="Vite logo" />
-          </a>
-          <a href="https://lit.dev" target="_blank">
-            <img src=${litLogo} class="logo lit" alt="Lit logo" />
-          </a>
-        </div>
-        <slot></slot>
-        <div class="card">
-          <button @click=${this._onClick} part="button">count is ${this.count}</button>
-        </div>
-        <p class="read-the-docs">${this.docsHint}</p>
+        <sl-tab-group>
+          <sl-tab slot="nav" panel="general">General</sl-tab>
+          <sl-tab slot="nav" panel="custom">Custom</sl-tab>
+          <sl-tab slot="nav" panel="advanced">Advanced</sl-tab>
+          <sl-tab slot="nav" panel="disabled" disabled>Disabled</sl-tab>
+
+          <sl-tab-panel name="general">This is the general tab panel.</sl-tab-panel>
+          <sl-tab-panel name="custom">This is the custom tab panel.</sl-tab-panel>
+          <sl-tab-panel name="advanced">This is the advanced tab panel.</sl-tab-panel>
+          <sl-tab-panel name="disabled">This is a disabled tab panel.</sl-tab-panel>
+        </sl-tab-group>
       </auth-context-provider>
-    `
+    `;
   }
 
-  private _onClick() {
-    this.count++
-  }
-
-  static styles = css`
-    :host {
-      max-width: 1280px;
-      margin: 0 auto;
-      padding: 2rem;
-      text-align: center;
-    }
-
-    .logo {
-      height: 6em;
-      padding: 1.5em;
-      will-change: filter;
-      transition: filter 300ms;
-    }
-
-    .logo:hover {
-      filter: drop-shadow(0 0 2em #646cffaa);
-    }
-
-    .logo.lit:hover {
-      filter: drop-shadow(0 0 2em #325cffaa);
-    }
-
-    .card {
-      padding: 2em;
-    }
-
-    .read-the-docs {
-      color: #888;
-    }
-
-    ::slotted(h1) {
-      font-size: 3.2em;
-      line-height: 1.1;
-    }
-
-    a {
-      font-weight: 500;
-      color: #646cff;
-      text-decoration: inherit;
-    }
-
-    a:hover {
-      color: #535bf2;
-    }
-
-    button {
-      border-radius: 8px;
-      border: 1px solid transparent;
-      padding: 0.6em 1.2em;
-      font-size: 1em;
-      font-weight: 500;
-      font-family: inherit;
-      background-color: #1a1a1a;
-      cursor: pointer;
-      transition: border-color 0.25s;
-    }
-
-    button:hover {
-      border-color: #646cff;
-    }
-
-    button:focus,
-    button:focus-visible {
-      outline: 4px auto -webkit-focus-ring-color;
-    }
-
-    @media (prefers-color-scheme: light) {
-      a:hover {
-        color: #747bff;
-      }
-
-      button {
-        background-color: #f9f9f9;
-      }
-    }
-  `
+  // static styles = css`
+  //   h1 {
+  //     font-size: var(--lumo-font-size-l);
+  //     left: var(--lumo-space-l);
+  //     margin: 0;
+  //     position: absolute;
+  //   }
+  //
+  // `
 }
 
 declare global {
--- litExample/src/tab-bar.ts
+++ litExample/src/tab-bar.ts
@@ -1,19 +1,19 @@
-import { customElement, state } from 'lit/decorators.js'
-import { LitElement, html } from 'lit'
-import '@vaadin/tabs'
-
-@customElement('tab-bar')
-export class TabBar extends LitElement {
-
-  render() {
-    return html`
-      <vaadin-tabs>
-        <vaadin-tab>Public</vaadin-tab>
-        <vaadin-tab>Secured</vaadin-tab>
-        <vaadin-tab>Config App</vaadin-tab>
-        <vaadin-tab>Admin App</vaadin-tab>
-        <vaadin-tab>User App</vaadin-tab>
-      </vaadin-tabs>
-    `
-  }
-}
+// import { customElement, state } from 'lit/decorators.js'
+// import { LitElement, html } from 'lit'
+// import '@vaadin/tabs'
+//
+// @customElement('tab-bar')
+// export class TabBar extends LitElement {
+//
+//   render() {
+//     return html`
+//       <vaadin-tabs>
+//         <vaadin-tab>Public</vaadin-tab>
+//         <vaadin-tab>Secured</vaadin-tab>
+//         <vaadin-tab>Config App</vaadin-tab>
+//         <vaadin-tab>Admin App</vaadin-tab>
+//         <vaadin-tab>User App</vaadin-tab>
+//       </vaadin-tabs>
+//     `
+//   }
+// }
