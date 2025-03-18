7c5e443f7c43f483692702c516a33125709e9ae0
Allan Brighton <Allan.Brighton@t-online.de>
2025-03-18 16:02:40 +0100

    Testing Jenkins file

--- scripts/install_node.sh
+++ scripts/install_node.sh
@@ -1,13 +1,12 @@
 #!/bin/bash
 
-set -x
 rm -rf $HOME/.nvm
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.2/install.sh | bash
 . $HOME/.nvm/nvm.sh
 nvm install v22.14.0
 npm install -g npm@11.2.0
 . $HOME/.nvm/nvm.sh
-echo $PATH
+echo "XXX HOME=$HOME, PATH=$PATH"
 which npm
 which node
 npm --version
